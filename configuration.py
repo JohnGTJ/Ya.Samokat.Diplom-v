16b1099bac98437f1c95c47ca15419e706a735d9
JohnGTJ <160781160+JohnGTJ@users.noreply.github.com>
2025-03-10 10:54:24 +0300

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://35b0296f-cf21-49cd-be18-ca717065c450.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://49f1d70d-98c4-4ec2-9405-d7ace2368294.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/api/v1/orders/track"
